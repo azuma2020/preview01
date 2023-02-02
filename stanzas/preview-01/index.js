76f496adb782ef384ac7c09b231040d4ab1314d0
Ryuzo Azuma <azuma.ryuuzou@advancesoft.jp>
2023-02-02 16:50:22 +0900

    Add value is...

--- stanzas/preview-01/index.js
+++ stanzas/preview-01/index.js
@@ -2,13 +2,29 @@ import Stanza from 'togostanza/stanza';
 
 export default class Preview01 extends Stanza {
   async render() {
-    this.renderTemplate(
-      {
-        template: 'stanza.html.hbs',
-        parameters: {
-          greeting: `Hello, ${this.params['say-to']}!`
-        }
-      }
-    );
+    this.renderTemplate({
+      template: 'stanza.html.hbs',
+      parameters: {
+        message: `The value is ${this.params.data.value}.`,
+      },
+    });
   }
+//  async render() {
+//    this.renderTemplate({
+//      template: 'stanza.html.hbs',
+//      parameters: {
+//        message: this.params.flag ? 'yes' : 'no',
+//      },
+//    });
+//  }
+//  async render() {
+//    this.renderTemplate(
+//      {
+//        template: 'stanza.html.hbs',
+//        parameters: {
+//          greeting: `Hello, ${this.params['say-to']}!`
+//        }
+//      }
+//    );
+//  }
 }
