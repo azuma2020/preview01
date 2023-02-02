7abb0c680b1a6c9b8932913d99092bcd2f4caaea
Ryuzo Azuma <azuma.ryuuzou@advancesoft.jp>
2023-02-02 12:23:42 +0900

    Add hello stanza

---
+++ stanzas/preview-01/README.md
@@ -0,0 +1,3 @@
+# Preview 01
+
+Stanza description goes here. Edit `stanzas/preview-01/README.md` to update.
---
+++ stanzas/preview-01/index.js
@@ -0,0 +1,14 @@
+import Stanza from 'togostanza/stanza';
+
+export default class Preview01 extends Stanza {
+  async render() {
+    this.renderTemplate(
+      {
+        template: 'stanza.html.hbs',
+        parameters: {
+          greeting: `Hello, ${this.params['say-to']}!`
+        }
+      }
+    );
+  }
+}
